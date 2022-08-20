f5914bf67c25f99c82bd103a1a9ec1b9a88aa682
KTeam-Devs <kirito234875@gmail.com>
2022-08-20 16:21:54 +0530

    lexer

--- src/include/token.h
+++ src/include/token.h
@@ -11,10 +11,22 @@ typedef struct TOKEN_STRUCT{
         TOKEN_EQUALS,
         TOKEN_LPAREN,
         TOKEN_RPAREN,
+        TOKEN_LBRACE,
+        TOKEN_RBRACE,
+        TOKEN_COLON,
+        TOKEN_COMMA,
+        TOKEN_LT,
+        TOKEN_GT,
+        TOKEN_RIGHT_ARROW,
         TOKEN_INT,
+        TOKEN_SEMI,
 
     } type;
 
 } token_T;
+
+token_T* init_token(char* value, int type);
+
+
 
 #endif
