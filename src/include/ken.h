110d7dd4ea6e13977bb24ca2e5a68d1dff238521
KTeam-Devs <kirito234875@gmail.com>
2022-08-20 16:32:32 +0530

    lexer

---
+++ src/include/ken.h
@@ -0,0 +1,6 @@
+#ifndef KEN_H
+#define KEN_H
+
+void ken_compile(char* src);
+
+#endif
---
+++ src/ken.c
@@ -0,0 +1,6 @@
+#include "include/ken.h"
+#include "include/lexer.h"
+
+void ken_compile(char* src) {
+    lexer_T* lexer = init_lexer(src);
+}
