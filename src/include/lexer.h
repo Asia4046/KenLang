982dbd53a7b1a06d7937f361aa21a032f2ea4f6d
KTeam-Devs <kirito234875@gmail.com>
2022-08-20 17:39:36 +0530

    lexer

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -2,10 +2,12 @@
 #define KEN_LEXER_H
 
 #include "token.h"
+#include <stdio.h>
 
 typedef struct LEXER_STRUCT {
 
     char* src;
+    size_t src_size;
     char c;
     unsigned int i;
 
@@ -14,6 +16,10 @@ typedef struct LEXER_STRUCT {
 lexer_T* init_lexer(char* src);
 
 void lexer_advance(lexer_T* lexer);
+
+void lexer_skip_whitespace(lexer_T* lexer);
+
+token_T* lexer_parse_id(lexer_T* lexer);
 
 token_T* lexer_next_token(lexer_T* lexer);
 
